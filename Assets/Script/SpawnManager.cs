5febc4d6c54f501feb1be3beca44295e8aaf8fdb
watanabeaina <aina0721omiomi@yahoo.com>
2023-05-20 16:29:36 +0900

    \unit3のゲーム製作途中

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //面倒なのでprivateは省略してます
+    [SerializeField] GameObject obstaclePrefab;//障害物プレハブ
+    Vector3 spawnPos = new Vector3(25,0,0);//スポーンする場所
+    float elapssedTime;//経過時間
+    // Start is called before the first frame update
+    void Update()
+    {
+        elapssedTime += Time.deltaTime;//毎Fの時間を足していく
+        //経過時間が2秒を超えたら
+        if(elapssedTime > 2.0f)
+        {
+            Instantiate(obstaclePrefab, spawnPos, transform.rotation);
+            elapssedTime = 0.0f;//経過時間リセット
+        } 
+    }
+
+    // Update is called once per frame
+}
