44631197f591e5885284fa994f7665dd43b57d7a
watanabeaina <aina0721omiomi@yahoo.com>
2023-05-27 16:22:22 +0900

    レッスン3.3まで終了

--- Assets/Script/MoveLeft.cs
+++ Assets/Script/MoveLeft.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     [SerializeField] float speed = 30;//Groundが動く速さ
+    PlayerController playerControllerScript;
+    float leftBound = -15;//左の限界値
+    //実は、PlayereControllerというのも、Rigidbodyとかと同様に「クラス」なので、型として宣言ができる！
+    //使いい方はいつも同じで、<型名> 変数名 の順序
+    //(例) Rigidbody rb;
+    //(例) int score;
+    //特にpublic等にしたい場合は、 (例) public int score;
+    //などとするが、ふつうはpublicにしない。とにかく、<型名>＋変数名の順序！
     // Start is called before the first frame update
     void Start()
-    {
-       
+    {//private補われるが、あってもなくても同じ意味
+       playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        //まず、左辺は頭で宣言しただけなので、実態が不明
+        //そこで代入が必要。
+        //PlayerControllerを持っているのはPlayer本人。なので、まず見つける(Find)する必要がある
+        //それがGameObject.Findです。ただし、Findの引数には、「一字一句」同じ名前を入れる必要があります
+        //なので、"Playe"とか誤字になるとバグります。
+        //GameObject.Find("Player")がプレイヤー自信を表すと思いましょう。
+        //そうすると、そいつがもっているPlayerControllerほしいですね。
+        //それは、GetComponent<型名>()という方法で取ってこれます。
+        //実際、みなさんはroll a ballをつくるときに、playerのRigidbodを取ってくるときに一回これを書いています。
     }
 
     // Update is called once per frame
     void Update()
     {
+        //ゲームオーバー状態でなければ、backgroundを動かす
+        if(playerControllerScript.gameOver == false) { 
         transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        //左の限界値(leftBound)よりも左に行ってしまったら…かつ、左行き過ぎたやつが障害物のタグ持ちなら
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            //障害物は消してしまう
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //privateは省略可能
+    Rigidbody rb;
+    [SerializeField] float gravityModifier;//重力値調整用
+    [SerializeField] float jumpForce;//ジャンプ力
+    [SerializeField] bool isOnGround;//地面についているか
+    public bool gameOver;//何も書かなければprivateです
+    Animator playerAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スペースキーが押されたて、かつ、地面にいたら、ゲームオーバーではないなら
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) { 
+            //上へ力を加える
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;//ジャンプした＝地面にいない
+            playerAnim.SetTrigger("Jump_trig");//ジャンプアニメ発動準備
+        }
+    }
+    //衝突が起きたら実行
+    private void OnCollisionEnter(Collision collision)
+    {
+        //ぶつかった相手(collision)のタグがGroundなら
+        if (collision.gameObject.CompareTag("Ground")) { 
+            isOnGround = true;//地面についている状態に変更
+        }
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;//ゲームオーバーにしてみる
+            playerAnim.SetBool("Death_b", true);//ここで死亡状態ｂにする。(Death_bとかいうのは本来自分で定義できる)
+            //playerAnim.SetInteger("DeathType_int", 1);//integerは整数の意味。死亡タイプ？を一番目のやつにする的な。
+        }
+    }
+}
